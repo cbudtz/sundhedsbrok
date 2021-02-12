5c4e0c4cb35836e05724edcdb6316eb0efc11ee3
Christian Budtz <chrbudtz@gmail.com>
2021-02-12 10:21:48 +0100

    Now with favicon

--- web/pages/index.js
+++ web/pages/index.js
@@ -6,6 +6,7 @@ import Blockquote from "../components/Blockquote";
 import {fetchAPI, postAPI} from "../lib/api";
 import {Formik} from "formik";
 import {useRouter} from "next/router";
+import Head from "next/head";
 
 function Home({broks}) {
     const carouselStyle={
@@ -16,10 +17,30 @@ function Home({broks}) {
 
     const router =  useRouter();
     return (<>
+        <Head>
+            <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+            <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
+            <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
+            <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
+            <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
+            <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
+            <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
+            <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+            <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/manifest.json"/>
+            <meta name="msapplication-TileColor" content="#ffffff"/>
+            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+            <meta name="theme-color" content="#ffffff"/>
+
+        </Head>
         <Container>
             <Row>
                 <Col md={{span: 6, offset:3}} style={{textAlign:"center", marginTop:-50, marginBottom:-50}}>
-                <img align={"center"} src={"/sundhedsbrok.png"} alt={"Sundhedsbrok"}/>
+                    <img align={"center"} src={"/sundhedsbrok.png"} alt={"Sundhedsbrok"}/>
                 </Col>
             </Row>
             <Row>
